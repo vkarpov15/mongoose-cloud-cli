0f3db76632c385523b7cc65bf74c7d3a841efc28
Valeri Karpov <val@karpov.io>
2020-08-01 14:08:42 -0400

    fix: only show relevant deployment properties

--- src/deployment.js
+++ src/deployment.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const axios = require('axios');
+const pick = require('lodash.pick');
 const prettyjson = require('prettyjson');
 
 const url = 'http://api.mongoosecloud.io/api';
 const authorization = process.env.API_KEY;
+const deploymentProps = ['organizationName', 'repoName', 'branch', 'status', 'urls', 'mongodb'];
 
 module.exports = async function deployment(deploymentId) {
   const opts = { headers: { authorization } };
   const { deployment, logs } = await axios.get(`${url}/deployment/${deploymentId}`, opts).
     then(res => res.data);
   
-  console.log(prettyjson.render({ ...deployment, logs }));
+  console.log(prettyjson.render({ ...pick(deployment, deploymentProps), logs }));
 };
